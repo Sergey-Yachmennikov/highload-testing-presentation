641a89451edb82ef32d0d02b938ee1253f3592ac
Sergey-Yachmennikov <otsmmm2013@gmail.com>
2026-05-10 08:46:27 +0300

    initial commit

---
+++ src/main/java/com/highload/highload_testing_perentation/HighloadTestingPerentationApplication.java
@@ -0,0 +1,13 @@
+package com.highload.highload_testing_perentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighloadTestingPerentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighloadTestingPerentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/highload/highload_testing_perentation/HighloadTestingPerentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highload.highload_testing_perentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighloadTestingPerentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
